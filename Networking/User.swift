9e6e987ed303fe9b9630328bf0de7e5944c316ed
Anton Potapov <anton.potapov@omega-r.com>
2021-03-09 14:51:32 +0300

    JSON-model for User

---
+++ Networking/User.swift
@@ -0,0 +1,50 @@
+//
+//  User.swift
+//  GitHubStarWatchdog
+//
+//  Created by Anton Potapov on 09.03.2021.
+//
+
+import Foundation
+
+struct User {
+    let login: String
+    let id: Int64
+    let nodeId: String
+    let avatarUrl: String
+    let gravatarId: String
+    let url: String
+    let htmlUrl: String
+    let followersUrl: String
+    let followingUrl: String
+    let gistsUrl: String
+    let starredUrl: String
+    let subscriptionsUrl: String
+    let organizationsUrl: String
+    let reposUrl: String
+    let eventsUrl: String
+    let receivedEventsUrl: String
+    let type: String
+    let siteAdmin: Bool
+    
+    enum UserCodingKeys: String, CodingKey {
+        case login
+        case id
+        case nodeId = "node_id"
+        case avatarUrl = "avatar_url"
+        case gravatarId = "gravatar_id"
+        case url
+        case htmlUrl = "html_url"
+        case followersUrl = "followers_url"
+        case followingUrl = "following_url"
+        case gistsUrl = "gists_url"
+        case starredUrl = "starred_url"
+        case subscriptionsUrl = "subscriptions_url"
+        case organizationsUrl = "organizations_url"
+        case reposUrl = "repos_url"
+        case eventsUrl = "events_url"
+        case receivedEventsUrl = "received_events_url"
+        case type
+        case siteAdmin = "site_admin"
+    }
+}
