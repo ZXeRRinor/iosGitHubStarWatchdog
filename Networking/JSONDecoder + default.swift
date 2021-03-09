dce93533e7005b07faf066f156cf3b7d6e93a922
Anton Potapov <anton.potapov@omega-r.com>
2021-03-09 16:22:42 +0300

    Added extension for JSONDecoder

---
+++ Networking/JSONDecoder + default.swift
@@ -0,0 +1,16 @@
+//
+//  JSONDecoder + default.swift
+//  GitHubStarWatchdog
+//
+//  Created by Anton Potapov on 09.03.2021.
+//
+
+import Foundation
+
+public extension JSONDecoder {
+     static var `default`: JSONDecoder {
+        let decoder = JSONDecoder()
+        decoder.dateDecodingStrategy = .formatted(.default)
+        return decoder
+    }
+}
