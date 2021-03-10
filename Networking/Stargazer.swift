adcf6507dd998e08ebade995991d55bd5147924f
Anton Potapov <anton.potapov@omega-r.com>
2021-03-10 13:42:33 +0300

    Deleted useless part

--- Networking/Stargazer.swift
+++ Networking/Stargazer.swift
@@ -10,8 +10,4 @@ import Foundation
 struct Stargazer: Decodable {
     let starredAt: Date
     let user: User
-//    enum StargazerCodingKeys: String, CodingKey {
-//        case starredAt = "starred_at"
-//        case user
-//    }
 }
