7379588b559bced31ec1127c81646881723991c2
Anton Potapov <anton.potapov@omega-r.com>
2021-03-10 15:50:55 +0300

    Turned off unused parts

--- Networking/Repository.swift
+++ Networking/Repository.swift
@@ -8,49 +8,58 @@
 import Foundation
 
 struct Repository: Decodable {
-    let id: Int
-    let nodeId, name, fullName: String
-    let `private`: Bool
-    let owner: User
-    let htmlUrl: String
-    let description: String?
-    let fork: Bool
-    let url, forksUrl: String
-    let keysUrl, collaboratorsUrl: String
-    let teamsUrl, hooksUrl: String
-    let issueEventsUrl: String
-    let eventsUrl: String
-    let assigneesUrl, branchesUrl: String
-    let tagsUrl: String
-    let blobsUrl, gitTagsUrl, gitRefsUrl, treesUrl: String
-    let statusesUrl: String
-    let languagesUrl, stargazersUrl, contributorsUrl, subscribersUrl: String
-    let subscriptionUrl: String
-    let commitsUrl, gitCommitsUrl, commentsUrl, issueCommentUrl: String
-    let contentsUrl, compareUrl: String
-    let mergesUrl: String
-    let archiveUrl: String
-    let downloadsUrl: String
-    let issuesUrl, pullsUrl, milestonesUrl, notificationsUrl: String
-    let labelsUrl, releasesUrl: String
-    let deploymentsUrl: String
-    let createdAt, updatedAt, pushedAt: Date
-    let gitUrl, sshUrl: String
-    let cloneUrl: String
-    let svnUrl: String
-    let homepage: String?
-    let size, stargazersCount, watchersCount: Int
-    let language: String?
-    let hasIssues, hasProjects, hasDownloads, hasWiki: Bool
-    let hasPages: Bool
-    let forksCount: Int
-    let mirrorUrl: String?
-    let archived, disabled: Bool
-    let openIssuesCount: Int
-    let license: String?
-    let forks, openIssues, watchers: Int
-    let defaultBranch: String
-    let tempCloneToken: String?
-    let organization: User
-    let networkCount, subscribersCount: Int?
+//    let id: Int
+//    let nodeId, name, fullName: String
+    
+//    let `private`: Bool
+    
+//    let owner: User
+//    let htmlUrl: String
+    
+//    let description: String?
+//    let fork: Bool
+//    let url, forksUrl: String
+//    let keysUrl, collaboratorsUrl: String
+//    let teamsUrl, hooksUrl: String
+//    let issueEventsUrl: String
+//    let eventsUrl: String
+//    let assigneesUrl, branchesUrl: String
+//    let tagsUrl: String
+//    let blobsUrl, gitTagsUrl, gitRefsUrl, treesUrl: String
+//    let statusesUrl: String
+//    let languagesUrl, stargazersUrl, contributorsUrl, subscribersUrl: String
+//    let subscriptionUrl: String
+//    let commitsUrl, gitCommitsUrl, commentsUrl, issueCommentUrl: String
+//    let contentsUrl, compareUrl: String
+//    let mergesUrl: String
+//    let archiveUrl: String
+//    let downloadsUrl: String
+//    let issuesUrl, pullsUrl, milestonesUrl, notificationsUrl: String
+//    let labelsUrl, releasesUrl: String
+//    let deploymentsUrl: String
+//    let createdAt, updatedAt, pushedAt: Date
+//    let gitUrl, sshUrl: String
+//    let cloneUrl: String
+//    let svnUrl: String
+//    let homepage: String?
+//    let size: Int
+    
+    let stargazersCount: Int
+    
+//    let watchersCount: Int
+//    let language: String?
+//    let hasIssues, hasProjects, hasDownloads, hasWiki: Bool
+//    let hasPages: Bool
+//    let forksCount: Int
+//    let mirrorUrl: String?
+//    let archived, disabled: Bool
+//    let openIssuesCount: Int
+//    let license: License?
+//    let forks, openIssues, watchers: Int
+//    let defaultBranch: String
+//    let tempCloneToken: String?
+    
+//    let organization: User
+    
+//    let networkCount, subscribersCount: Int?
 }
--- Networking/User.swift
+++ Networking/User.swift
@@ -9,21 +9,23 @@ import Foundation
 
 struct User: Decodable {
     let login: String
-    let id: Int64
-    let nodeId: String
-    let avatarUrl: String
-    let gravatarId: String
-    let url: String
-    let htmlUrl: String
-    let followersUrl: String
-    let followingUrl: String
-    let gistsUrl: String
-    let starredUrl: String
-    let subscriptionsUrl: String
-    let organizationsUrl: String
-    let reposUrl: String
-    let eventsUrl: String
-    let receivedEventsUrl: String
-    let type: String
-    let siteAdmin: Bool
+//    let id: Int64
+//    let nodeId: String
+//    let avatarUrl: String
+//    let gravatarId: String
+//    let url: String
+    
+//    let htmlUrl: String
+//    let followersUrl: String
+//    let followingUrl: String
+//    let gistsUrl: String
+//    let starredUrl: String
+//    let subscriptionsUrl: String
+//    let organizationsUrl: String
+//    let reposUrl: String
+//    let eventsUrl: String
+//    let receivedEventsUrl: String
+    
+//    let type: String
+//    let siteAdmin: Bool
 }
