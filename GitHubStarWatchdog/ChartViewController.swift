90ac4914abe2e4d7efa6fe375559f35da1c5f7d1
Anton Potapov <anton.potapov@omega-r.com>
2021-03-11 10:49:35 +0300

    Added chart-screen

---
+++ GitHubStarWatchdog/ChartViewController.swift
@@ -0,0 +1,97 @@
+//
+//  ChartViewController.swift
+//  GitHubStarWatchdog
+//
+//  Created by Anton Potapov on 10.03.2021.
+//
+import UIKit
+import Charts
+
+class ChartViewController: UIViewController, ChartViewDelegate {
+    var barChart = BarChartView()
+    var stargazers: [Stargazer] = []
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        barChart.delegate = self
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        barChart.frame = CGRect(x: 0, y: 0, width: self.view.frame.size.width, height: self.view.frame.size.height)
+        barChart.center = view.center
+        view.addSubview(barChart)
+        customizeBarChart()
+        let calendar = Calendar.current
+        var dateComponent = DateComponents()
+        dateComponent.month = -11
+        let startDate = calendar.date(byAdding: dateComponent, to: Date())!
+        let currentYear = calendar.component(.year, from: Date())
+        var dataSet = [Int: [Stargazer]]()
+        stargazers.forEach {
+            if $0.starredAt >= startDate {
+                var index = calendar.component(.month, from: $0.starredAt)
+                let year = calendar.component(.year, from: $0.starredAt)
+                if year == currentYear {
+                    index += 12
+                }
+                if dataSet[index] != nil {
+                    dataSet[index]!.append($0)
+                } else {
+                    dataSet[index] = [$0]
+                }
+            }
+        }
+        var result: [BarChartDataEntry] = []
+        dataSet.keys.forEach { i in
+            print(i)
+            if let stargazerGroup = dataSet[i] {
+                result.append(BarChartDataEntry(x: Double(i), y: Double(stargazerGroup.count)))
+            }
+        }
+        
+        var testEntries = [BarChartDataEntry]()
+        for i in 0...10 {
+            testEntries.append(BarChartDataEntry(x: Double(i), y: Double(i * 10)))
+        }
+        setChartEntries(result)
+    }
+    
+    func setChartEntries(_ entries: [BarChartDataEntry]) {
+        let chart = barChart
+        chart.setScaleEnabled(false)
+        chart.pinchZoomEnabled = false
+        
+        let data = BarChartData(dataSet: BarChartDataSet(entries: entries, label: "data"))
+        chart.data = data
+        chart.invalidateIntrinsicContentSize()
+    }
+    
+    func customizeBarChart() {
+        let xAxis = barChart.xAxis
+//        xAxis.labelPosition = XAxis.XAxisPosition.BOTTOM
+        xAxis.labelPosition = XAxis.LabelPosition.bottom
+        xAxis.drawGridLinesEnabled = false
+        xAxis.granularity = 1
+        xAxis.labelCount = 12
+//        xAxis.axisMinimum = start.toFloat() - 0.5
+        barChart.fitBars = true
+        xAxis.valueFormatter = CustomValueFormatter()
+    }
+}
+
+extension ChartViewController {
+    static func build(with stargazers: [Stargazer]) -> ChartViewController {
+        let sb = UIStoryboard(name: "Chart", bundle: nil)
+        let controller = sb.instantiateViewController(identifier: "ChartViewController") as! ChartViewController
+        controller.stargazers = stargazers
+        return controller
+    }
+}
+
+class CustomValueFormatter: AxisValueFormatter {
+    func stringForValue(_ value: Double, axis: AxisBase?) -> String {
+        let monthComponents = Calendar.current.shortMonthSymbols
+        return monthComponents[Int(value - 1) % 12]
+    }
+}
