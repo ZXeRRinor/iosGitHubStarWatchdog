9071dde31d51eed2d4da8d362ed680160db607c4
Anton Potapov <anton.potapov@omega-r.com>
2021-03-11 10:50:29 +0300

    Navigation to chart-screen

--- GitHubStarWatchdog/ViewController.swift
+++ GitHubStarWatchdog/ViewController.swift
@@ -6,9 +6,26 @@
 //
 
 import UIKit
+import Moya
+import Promises
 
 class ViewController: UIViewController {
-
+    var stargazers: [Stargazer] = []
+    var stargazersNeeded: Int = 0
+    var stargazersLoaded: Int = 0 {
+        didSet {
+            print(stargazersNeeded != 0 && stargazersNeeded <= stargazersLoaded)
+            print(stargazersNeeded)
+            print(stargazersLoaded)
+            if(stargazersNeeded != 0 && stargazersNeeded <= stargazersLoaded) {
+                stargazersNeeded = 0
+                stargazersLoaded = 0
+                goToChart()
+            }
+        }
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
@@ -19,8 +36,27 @@ class ViewController: UIViewController {
     @IBOutlet weak var repoNameField: UITextField!
     
     @IBAction func onLoadButtonClick(_ sender: UIButton) {
-        print(userNameField?.text ?? "")
-        print(repoNameField?.text ?? "")
+        let service = GitHubApiService()
+        if let repoName = repoNameField.text, let userName = userNameField.text {
+            let repo = service.getRepository(withName: repoName, by: userName)
+            repo.then { r in
+                self.stargazersNeeded = r.stargazersCount
+                let iterations = Int(ceil(Double(r.stargazersCount) / 100))
+                for i in 0..<iterations {
+                    service.getStargazers(for: repoName, by: userName, fromPage: i).then { stargazers in
+                        self.stargazers.append(contentsOf: stargazers)
+                        self.stargazersLoaded += stargazers.count
+                    }
+                }
+            }
+        } else {
+            print("incorrect username or reponame")
+        }
+    }
+    
+    func goToChart() {
+        let cvc = ChartViewController.build(with: stargazers)
+        show(cvc, sender: nil)
     }
 }
 
