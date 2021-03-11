0ab9cbc94905167d407978dffd354497ff5541e6
Anton Potapov <anton.potapov@omega-r.com>
2021-03-11 11:29:44 +0300

    Added UserList storyboard and UserListViewController with builder

---
+++ GitHubStarWatchdog/UserListViewController.swift
@@ -0,0 +1,23 @@
+//
+//  UserListViewController.swift
+//  GitHubStarWatchdog
+//
+//  Created by Anton Potapov on 11.03.2021.
+//
+
+import UIKit
+
+class UserListViewController: UIViewController {
+    var users: [User] = []
+    
+    
+}
+
+extension UserListViewController {
+    func build(for users: [User]) -> UserListViewController {
+        let sb = UIStoryboard(name: "UserList", bundle: nil)
+        let controller = sb.instantiateViewController(identifier: "UserListViewController") as! UserListViewController
+        controller.users = users
+        return controller
+    }
+}
