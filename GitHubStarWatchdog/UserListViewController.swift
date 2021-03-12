129fdce6b6a983b94e15163c3fd1de2f35a99a4c
Anton Potapov <anton.potapov@omega-r.com>
2021-03-12 13:58:56 +0300

    TableView for UserList storyboard

--- GitHubStarWatchdog/UserListViewController.swift
+++ GitHubStarWatchdog/UserListViewController.swift
@@ -7,13 +7,33 @@
 
 import UIKit
 
-class UserListViewController: UIViewController {
+class UserListViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     var users: [User] = []
     
+    @IBOutlet weak var userListTable: UITableView!
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        print(section)
+        return users.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell: UITableViewCell = self.userListTable!.dequeueReusableCell(withIdentifier: "cell")! as UITableViewCell
+        cell.textLabel!.text = self.users[indexPath.row].login
+        return cell
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        userListTable?.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        userListTable?.delegate = self
+        userListTable?.dataSource = self
+    }
 }
 
 extension UserListViewController {
-    static func build(for users: [User]) -> UserListViewController {
+    static func build(with users: [User]) -> UserListViewController {
         let sb = UIStoryboard(name: "UserList", bundle: nil)
         let controller = sb.instantiateViewController(identifier: "UserListViewController") as! UserListViewController
         controller.users = users
